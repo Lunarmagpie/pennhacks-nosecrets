71097c9ecba466efa91cadb93d74a477ef45d026
Arogya Dahal <arogya@ist-487.eduroam.wlan.private.upenn.edu>
2023-09-09 18:54:42 -0400

    Front

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Main2 from "./Main2";
 import Menubar from "./components/menubar";
 import Main from "./components/main";
 import Settings from "./components/Settings"
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,18 +1,78 @@
+import { useEffect, useState, useId } from "react";
+
+const languages = [
+    "English",
+    "Bulgarian",
+    "Czech",
+    "Danish",
+    "German",
+    "Greek",
+    "Spanish",
+    "Estonian",
+    "Finnish",
+    "French",
+    "Hungarian",
+    "Indonesian",
+    "Italian",
+    "Japanese",
+    "Korean",
+    "Lithuanian",
+    "Latvian",
+    "Norwegian",
+    "Dutch",
+    "Polish",
+    "Portuguese",
+    "Romanian",
+    "Russian",
+    "Slovak",
+    "Slovenian",
+    "Swedish",
+    "Turkish",
+    "Ukrainian",
+    "Chinese",
+];
+
 export default function Settings() {
+    let [data, setData] = useState({"phone": "", "language": "English", "wList": "", "bList": ""});
+
+    useEffect(() => {
+        console.log(data);
+    }, [data])
+
     return (
         <form onSubmit={onSubmit}>
 
         <label for="phone_number">Phone Number:</label><br/>
-        <input type="text" id="phone_number" name="phone_number" value="" /><br/>
+        <input type="text" id="phone_number" onChange={(e) => {
+            let d = {...data};
+            d.phone = e.target.value;
+            setData(d)}} name="phone_number" value={data.phone} /><br/>
 
         <label for="language">Translate To:</label><br/>
-        <input type="text" id="language" name="language" value="" placeholder="English" /><br/>
 
+        <select onChange={e => {
+            let d = {...data};
+            d.language = e.target.value;
+            setData(d)
+        }}>
+            {languages.map(v => <option key={v}>{v}</option>)}
+        </select>
+        <br/>
         <label for="whitelist">Whitelist:</label><br/>
-        <input type="text" id="whitelist" name="whitelist" value="" placeholder="Work, School" /><br/>
+        <input onChange={(e) => {
+            let d = {...data};
+            d.wList = e.target.value
+            setData(d);
+        }} type="text" id="whitelist" name="whitelist" value={data.wList} placeholder="Work, School" /><br/>
 
         <label for="blacklist">Blacklist:</label><br/>
-        <input type="text" id="blacklist" name="blacklist" value="" placeholder="TikTok, X.com" /><br/>
+        <input 
+            onChange={(e) => {
+            let d = {...data};
+            d.bList = e.target.textContent
+            setData(d);
+        }} 
+        type="text" id="blacklist" name="blacklist" value={data.bList} placeholder="TikTok, X.com" /><br/>
 
         <input type="submit" value="Save"/>
 
